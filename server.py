489de1dfc854a1d814fc068e57880773c8514467
Roscoe <elysiaroscoe@gmail.com>
2021-10-31 22:42:05 -0700

    commit

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'survey time yall'
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/process', methods=['POST'])         
+def process():
+    print(request.form)
+    return redirect('/result')
+
+@app.route('/result')
+def result():
+    return render_template("result.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
